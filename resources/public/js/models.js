bc977873ce75834169e8223443eff7260e9ac4c6
Yair Iny <yair.iny@gmail.com>
2013-10-04 15:41:08 +1000

    updated ember side to demonstrate lazy loading of accounts

--- resources/public/js/models.js
+++ resources/public/js/models.js
@@ -6,7 +6,12 @@ define(['application'], function() {
 	var attr = DS.attr;
 
 	App.User = DS.Model.extend({
-		username: attr()
+		username: attr(),
+		accounts: DS.hasMany('account', {async: true})
+	});
+
+	App.Account = DS.Model.extend({
+		name: attr()
 	});
 
 });
